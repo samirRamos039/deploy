4bc62bb9cb601fb3fda9e82368c3770916300824
rzagza039@gmail.com <rzagza039@gmail.com>
2023-11-19 17:23:29 -0500

    test 2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 //import './style.css'
 import App from './App.vue'
+import test from './test.vue'
 
 createApp(App).mount('#app')
---
+++ src/test.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>hola a todos</h1>
+    <h1>hola a todos</h1>
+</template>
